fbcc874147ed915c19b74de319ec8e10af00f434
burczu <b.dybowski@gmail.com>
2017-06-19 17:03:46 +0200

    switched to data inside the json file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const events = [
-  {
-    id: 1,
-    name: 'meetJS Wrocław',
-    place: 'Monterail',
-    date: '2017-08-12',
-    time: '18:00'
-  },
-  {
-    id: 2,
-    name: 'DevOps Wrocław',
-    place: 'Mleczarnia',
-    date: '2017-08-14',
-    time: '19:30'
-  },
-  {
-    id: 3,
-    name: 'Wrocławska Grupa .NET',
-    place: 'Nietota',
-    date: '2017-08-20',
-    time: '18:30'
-  }
-];
+import events from './data/events.json';
 
 ReactDOM.render(<p>Your app goes here...</p>, document.getElementById('root'));
