9f851406be146a26615440726d43e5a271577ac3
burczu <b.dybowski@gmail.com>
2017-06-19 17:12:56 +0200

    switched to events.json

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import events from './data/events.json';
 import Events from './Events';
-
-const events = [
-  {
-    id: 1,
-    name: 'meetJS Wrocław',
-    place: 'Monterail',
-    date: '2017-05-12',
-    time: '18:00'
-  },
-  {
-    id: 2,
-    name: 'DevOps Wrocław',
-    place: 'Mleczarnia',
-    date: '2017-08-14',
-    time: '19:30'
-  },
-  {
-    id: 3,
-    name: 'Wrocławska Grupa .NET',
-    place: 'Nietota',
-    date: '2017-08-20',
-    time: '18:30'
-  }
-];
 
 ReactDOM.render(<Events events={events} />, document.getElementById('root'));
