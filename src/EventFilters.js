aab2103c802bc6b2dc3d891fa04a6f4355220eb2
burczu <b.dybowski@gmail.com>
2017-06-19 19:19:16 +0200

    placeholder added

--- src/EventFilters.js
+++ src/EventFilters.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const EventFilters = (props) => {
-  return <input type="text" onChange={props.onFilterChange} />
+  return <input type="text" onChange={props.onFilterChange} placeholder="filtruj..." />
 };
 
 export default EventFilters;
