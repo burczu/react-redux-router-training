bcb493bb6a91d3764c1e28cfcf891b820fa7e6e2
burczu <b.dybowski@gmail.com>
2017-06-19 14:24:02 +0200

    excercise initialization

--- src/Events.js
+++ src/Events.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
-const Events = (props) => {
-  return (
-    <ul>
-      {props.events.map(item => {
-        const date = new Date(item.date);
+class Events extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.events.map(item => {
+          const date = new Date(item.date);
 
-        if (date >= Date.now()) {
-          return (
-            <li key={item.id}>
-              <strong>{item.name}</strong><br />
-              Gdzie: {item.place}<br />
-              Kiedy: {item.date} - {item.time}
-            </li>
-          );
-        }
+          if (date >= Date.now()) {
+            return (
+              <li key={item.id}>
+                <strong>{item.name}</strong><br />
+                Gdzie: {item.place}<br />
+                Kiedy: {item.date} - {item.time}
+              </li>
+            );
+          }
 
-        return null;
-      })}
-    </ul>
-  );
+          return null;
+        })}
+      </ul>
+    );
+  }
 };
 
 export default Events;
