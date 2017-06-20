0ba497cab18b9acb97ecd5e5b2bfcefbbab27b95
burczu <b.dybowski@gmail.com>
2017-06-20 10:29:16 +0200

    excercise prep

--- src/Events.js
+++ src/Events.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Events extends React.Component {
+  static propTypes = {
+    events: PropTypes.array.isRequired
+  };
+
   render() {
     return (
       <ul>
