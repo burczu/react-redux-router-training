01347a56af5bbc340512ac10abd97d2faa9cd31c
burczu <b.dybowski@gmail.com>
2017-06-20 16:40:37 +0200

    missing reducer content added

---
+++ src/reducers/eventsReducer.js
@@ -0,0 +1,10 @@
+import * as constants from '../constants';
+import events from '../data/events.json';
+
+const initialState = {
+  events
+};
+
+export function eventsReducer(state = initialState, action) {
+  return state;
+}
