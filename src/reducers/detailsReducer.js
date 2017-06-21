8781fe59bb1c9a2999cd754f3e0bf1085b01dd85
burczu <b.dybowski@gmail.com>
2017-06-21 13:16:14 +0200

    getting data for details

--- src/reducers/detailsReducer.js
+++ src/reducers/detailsReducer.js
@@ -3,11 +3,20 @@ import events from '../data/events.json';
 
 const initialState = {
   events,
+  dataLoaded: false,
+  isLoading: false,
+  isError: false,
   event: {}
 };
 
 export function detailsReducer(state = initialState, action) {
   switch (action.type) {
+    case constants.DETAILS_GET_DETAILS:
+      return { ...state, isLoading: true };
+    case constants.DETAILS_GET_DETAILS_SUCCESS:
+      return { ...state, isLoading: false, event: action.payload.data, dataLoaded: true };
+    case constants.DETAILS_GET_DETAILS_ERROR:
+      return { ...state, isLoading: false, isError: true };
     case constants.DETAILS_SELECT_EVENT:
       const event = state.events.find(item => item.id === parseInt(action.payload.id, 10));
       return { ...state, event };
