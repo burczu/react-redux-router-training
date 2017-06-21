6ddf458942675654c947eff5065e9bca2c8161c2
burczu <b.dybowski@gmail.com>
2017-06-21 10:07:31 +0200

    redux for details view

--- src/reducers/detailsReducer.js
+++ src/reducers/detailsReducer.js
@@ -2,9 +2,16 @@ import * as constants from '../constants';
 import events from '../data/events.json';
 
 const initialState = {
+  events,
   event: {}
 };
 
 export function detailsReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case constants.DETAILS_SELECT_EVENT:
+      const event = state.events.find(item => item.id === parseInt(action.payload.id, 10));
+      return { ...state, event };
+    default:
+      return state;
+  }
 }
